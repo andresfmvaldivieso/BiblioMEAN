ba228edfd81d8e74ea13567b696859f93c0ef96a
andresfmvaldivieso <andresfmvaldivieso@gmail.com>
2021-11-12 16:06:12 -0500

    add de metodo login + hasheo de pass en register

--- Backend/controllers/clientes.js
+++ Backend/controllers/clientes.js
@@ -1,4 +1,7 @@
 import clientes from "../models/clientes.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import moment from "moment";
 
 const registerClientes = async (req, res) => {
   if (!req.body.name || !req.body.email || !req.body.password)
@@ -7,10 +10,12 @@ const registerClientes = async (req, res) => {
   const existingCliente = await clientes.findOne({ name: req.body.name });
   if (existingCliente) return res.status(400).send("The client already exist");
 
+  const hash = await bcrypt.hash(req.body.password, 10);
+
   const clienteSchema = new clientes({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hash,
     dbStatus: true,
   });
   const result = await clienteSchema.save();
@@ -44,10 +49,45 @@ const updateCliente = async (req, res) => {
     : res.status(200).send({ clienteUpdate });
 };
 
-const deleteCliente= async (req, res) => {
-   const clienteDelete =await clientes.findByIdAndDelete( {_id: req.params["_id"]});
-   return !clienteDelete
-   ? res.status(400).send("client no fount")
-   : res.status(200).send("Client delete")
-}
-export default { registerClientes, listClientes, updateCliente, deleteCliente };
+const deleteCliente = async (req, res) => {
+  const clienteDelete = await clientes.findByIdAndDelete({
+    _id: req.params["_id"],
+  });
+  return !clienteDelete
+    ? res.status(400).send("client no fount")
+    : res.status(200).send("Client delete");
+};
+
+const login = async (req, res) => {
+  if (!req.body.email || !req.body.password)
+    return res.status("400").send({ message: "Incomplete data" });
+
+  const existingUser = await clientes.findOne({ email: req.body.email });
+  if (!existingUser) return res.status(400).send("Wrong email or password");
+
+  const hash = await bcrypt.compare(req.body.password, existingUser.password);
+  if (!hash) return res.status(400).send("Wrong email ");
+
+  try {
+    return res.status(200).json({
+      token: jwt.sign(
+        {
+          _id: existingUser._id,
+          name: existingUser.name,
+          dbStatus: existingUser.dbStatus,
+          iat: moment().unix(),
+        },
+        process.env.SECRET_KEY_JWT
+      ),
+    });
+  } catch (e) {
+    return res.status(400).send({ message: "login Error" });
+  }
+};
+export default {
+  registerClientes,
+  listClientes,
+  updateCliente,
+  deleteCliente,
+  login,
+};
--- Backend/routes/clientes.js
+++ Backend/routes/clientes.js
@@ -3,6 +3,7 @@ import clientes from "../controllers/clientes.js"
 
 const router = express.Router();
 router.post("/registerCliente",clientes.registerClientes);
+router.post("/login", clientes.login);
 router.get("/listarClientes", clientes.listClientes);
 router.put("/updateCliente",clientes.updateCliente);
 router.delete("/deleteCliente/:_id",clientes.deleteCliente);
